0e8ddfb06d5809947cfd9a2026b58208390f5e5a
user <user@408-37>
2026-05-14 14:16:20 +0900

    feat: 블럭 정의 및 출력

---
+++ tetris/block.h
@@ -0,0 +1,6 @@
+#ifndef BLOCK_H
+#define BLOCK_H
+
+void PrintBlock(int a, int b, int c, int d);
+
+#endif
--- tetris/main.c
+++ tetris/main.c
@@ -1,8 +1,15 @@
-#include "game.h"
+#include "block.h"
+#include <stdio.h>
 
 int main(void) {
 
-    RunGame();
+    int a, b, c, d;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    scanf("%d", &d);
+
+    PrintBlock(a, b, c, d);
 
     return 0;
 }
