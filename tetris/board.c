1a1de15a9d3fc791507471b2d591daeaa54418cd
user <user@408-37>
2026-05-19 14:08:03 +0900

    feat: 보드 테두리 출력

---
+++ tetris/board.c
@@ -0,0 +1,29 @@
+﻿#include "board.h"
+#include <stdio.h>
+#include "console.h"
+
+#define BOARD_ROWS 20
+#define BOARD_COLS 10
+
+int board[BOARD_ROWS][BOARD_COLS];
+
+void DrawBoarder(void) {
+
+	for (int i = 0; i < BOARD_ROWS; i++) {
+
+		for (int j = 0; j < BOARD_COLS; j++) {
+			
+			if (i == 0 || i == BOARD_ROWS - 1) {
+				GotoXY(j * 2, i);
+				printf("-");
+			}
+			else {
+				if (j == 0 || j == BOARD_COLS - 1) {
+					GotoXY(j * 2, i);
+					printf("|");
+				}
+			}
+		}
+	}
+	
+}
--- tetris/main.c
+++ tetris/main.c
@@ -1,15 +1,9 @@
-#include "block.h"
+﻿#include "board.h"
 #include <stdio.h>
 
 int main(void) {
 
-    int a, b, c, d;
-    scanf_s("%d", &a);
-    scanf_s("%d", &b);
-    scanf_s("%d", &c);
-    scanf_s("%d", &d);
-
-    PrintBlock(a, b, c, d);
+    DrawBoarder();
 
     return 0;
 }
