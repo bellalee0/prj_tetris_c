dce2425bd750bf09b48ba022ad5aec31b66e2b33
user <user@408-28>
2026-05-21 14:14:57 +0900

    refactor: 블럭 재정의 - struct 사용

--- tetris/block.h
+++ tetris/block.h
@@ -1,6 +1,87 @@
-#ifndef BLOCK_H
+﻿#ifndef BLOCK_H
 #define BLOCK_H
 
-void PrintBlock(int a, int b, int c, int d);
+#include "console.h"
+
+typedef struct {
+	int shape[4][4];
+	Color color;
+} Tetromino;
+
+Tetromino BLOCK_I = {
+	.shape = {
+		0,0,0,0,
+		2,2,2,2,
+		0,0,0,0,
+		0,0,0,0
+	},
+	.color = SKYBLUE
+};
+
+Tetromino BLOCK_O = {
+	.shape = {
+		0,0,0,0,
+		0,2,2,0,
+		0,2,2,0,
+		0,0,0,0
+	},
+	.color = YELLOW
+};
+
+Tetromino BLOCK_T = {
+	.shape = {
+		0,0,0,0,
+		0,2,2,2,
+		0,0,2,0,
+		0,0,0,0
+	},
+	.color = PURPLE
+};
+
+Tetromino BLOCK_J = {
+	.shape = {
+		0,0,0,0,
+		2,2,2,0,
+		0,0,2,0,
+		0,0,0,0
+	},
+	.color = DARKBLUE
+};
+
+Tetromino BLOCK_L = {
+	.shape = {
+		0,0,0,0,
+		0,2,2,2,
+		0,2,0,0,
+		0,0,0,0
+	},
+	.color = DARKYELLOW
+};
+
+Tetromino BLOCK_S = {
+	.shape = {
+		0,0,0,0,
+		0,0,2,2,
+		0,2,2,0,
+		0,0,0,0
+	},
+	.color = GREEN
+};
+
+Tetromino BLOCK_Z = {
+	.shape = {
+		0,0,0,0,
+		0,2,2,0,
+		0,0,2,2,
+		0,0,0,0
+	},
+	.color = RED
+};
+
+typedef struct {
+	Tetromino tetromino;
+	int x;
+	int y;
+} ActiveBlock;
 
 #endif
