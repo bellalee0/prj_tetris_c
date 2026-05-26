b765985818b320da9b188088e57369d41e1fe410
user <user@408-37>
2026-05-26 13:28:12 +0900

    refactor: 블럭 정의 시 소스 파일 사용

---
+++ tetris/block.c
@@ -0,0 +1,72 @@
+﻿#include "block.h"
+#include <stdio.h>
+
+Tetromino BLOCK_I = {
+	.shape = {
+		0,0,0,0,
+		2,2,2,2,
+		0,0,0,0,
+		0,0,0,0
+	},
+	.color = SKYBLUE
+};
+
+Tetromino BLOCK_O = {
+	.shape = {
+		0,0,0,0,
+		0,2,2,0,
+		0,2,2,0,
+		0,0,0,0
+	},
+	.color = YELLOW
+};
+
+Tetromino BLOCK_T = {
+	.shape = {
+		0,0,0,0,
+		0,2,2,2,
+		0,0,2,0,
+		0,0,0,0
+	},
+	.color = PURPLE
+};
+
+Tetromino BLOCK_J = {
+	.shape = {
+		0,0,0,0,
+		2,2,2,0,
+		0,0,2,0,
+		0,0,0,0
+	},
+	.color = DARKBLUE
+};
+
+Tetromino BLOCK_L = {
+	.shape = {
+		0,0,0,0,
+		0,2,2,2,
+		0,2,0,0,
+		0,0,0,0
+	},
+	.color = DARKYELLOW
+};
+
+Tetromino BLOCK_S = {
+	.shape = {
+		0,0,0,0,
+		0,0,2,2,
+		0,2,2,0,
+		0,0,0,0
+	},
+	.color = GREEN
+};
+
+Tetromino BLOCK_Z = {
+	.shape = {
+		0,0,0,0,
+		0,2,2,0,
+		0,0,2,2,
+		0,0,0,0
+	},
+	.color = RED
+};
--- tetris/block.h
+++ tetris/block.h
@@ -8,75 +8,13 @@ typedef struct {
 	Color color;
 } Tetromino;
 
-Tetromino BLOCK_I = {
-	.shape = {
-		0,0,0,0,
-		2,2,2,2,
-		0,0,0,0,
-		0,0,0,0
-	},
-	.color = SKYBLUE
-};
-
-Tetromino BLOCK_O = {
-	.shape = {
-		0,0,0,0,
-		0,2,2,0,
-		0,2,2,0,
-		0,0,0,0
-	},
-	.color = YELLOW
-};
-
-Tetromino BLOCK_T = {
-	.shape = {
-		0,0,0,0,
-		0,2,2,2,
-		0,0,2,0,
-		0,0,0,0
-	},
-	.color = PURPLE
-};
-
-Tetromino BLOCK_J = {
-	.shape = {
-		0,0,0,0,
-		2,2,2,0,
-		0,0,2,0,
-		0,0,0,0
-	},
-	.color = DARKBLUE
-};
-
-Tetromino BLOCK_L = {
-	.shape = {
-		0,0,0,0,
-		0,2,2,2,
-		0,2,0,0,
-		0,0,0,0
-	},
-	.color = DARKYELLOW
-};
-
-Tetromino BLOCK_S = {
-	.shape = {
-		0,0,0,0,
-		0,0,2,2,
-		0,2,2,0,
-		0,0,0,0
-	},
-	.color = GREEN
-};
-
-Tetromino BLOCK_Z = {
-	.shape = {
-		0,0,0,0,
-		0,2,2,0,
-		0,0,2,2,
-		0,0,0,0
-	},
-	.color = RED
-};
+extern Tetromino BLOCK_I;
+extern Tetromino BLOCK_O;
+extern Tetromino BLOCK_T;
+extern Tetromino BLOCK_J;
+extern Tetromino BLOCK_L;
+extern Tetromino BLOCK_S;
+extern Tetromino BLOCK_Z;
 
 typedef struct {
 	Tetromino tetromino;
